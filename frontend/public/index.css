e3d0ac183982c02d30db0c0181f56b32c74656d9
shreyash-H <117390031+shreyash-H@users.noreply.github.com>
2025-12-03 22:26:26 +0530

    Create index.css

---
+++ frontend/public/index.css
@@ -0,0 +1,11 @@
+/* frontend/src/index.css */
+html, body, #root {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background: #fff;
+  color: #111;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: Inter, ui-sans-serif, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial;
+}
